aa8337c7923709753ca6b239a12e1d5ce37deced
mihran sargsyan <misargsyan@gmail.com>
2021-10-07 21:33:43 +0400

    Add Homework

--- src/Chapter3/Average.java
+++ src/Chapter3/Average.java
@@ -7,13 +7,12 @@ public class Average {
         double nums[] = {10.1, 11.2, 12.3, 13.4, 14.5};
 
         double result = 0;
-
-                for (int i = 0; i < 5; i++) {
-
+        int i;
+        for (i = 0; i < 5; i++)
             result = result + nums[i];
 
-            System.out.println("Mijin tvabanakany" + result / 5);
+        System.out.println("Mijin tvabanakany" + result / 5);
 
-        }
     }
 }
+
