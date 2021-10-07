72c9c1e21dc79a3d236cb411ae1a79b0d3087e4e
mihran sargsyan <misargsyan@gmail.com>
2021-10-07 21:05:09 +0400

    Add Homework

--- src/Chapter3/Example.java
+++ src/Chapter3/Example.java
@@ -11,14 +11,8 @@ public class Example {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 5; i++) {
-
-            for (int j = i; j < 4; j++) {
-
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
     }
 }
+
+
 
--- src/Homework/FigurePainter.java
+++ src/Homework/FigurePainter.java
@@ -3,7 +3,7 @@ package Homework;
 public class FigurePainter {
 
     public static void main(String[] args) {
-// Առաջին օրինակը
+        // Առաջին օրինակը
         for (int i = 0; i < 5; i++) {
 
             for (int j = 0; j < i; j++) {
@@ -12,6 +12,19 @@ public class FigurePainter {
             }
             System.out.println();
         }
+        // Երկրորդ օրինակը
+        System.out.println(); // իրարից առանձնացնելու համար
+        for (int i = 0; i < 4; i++) {
+            for (int j = i; j < 4; j++) {
+                System.out.print("  ");
+
+            }
+            for (int k = 4 - i; k < 5; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
         // Երրորդ օրինակը
         System.out.println(); // իրարից առանձնացնելու համար
 
