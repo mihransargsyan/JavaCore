be5bef568c60e74e4c99ba8af8bcc9c11a56f863
mihransargsyan <misargsyan@gmail.com>
2021-10-17 15:43:01 +0400

    Add Homework

---
+++ src/Chapter5/DoWhile.java
@@ -0,0 +1,13 @@
+package Chapter5;
+
+public class DoWhile {
+
+    public static void main(String[] args) {
+
+        int n = 10;
+        do {
+            System.out.println("takt " + n);
+            n--;
+        } while (n > 0);
+    }
+}
---
+++ src/Chapter5/ForTick.java
@@ -0,0 +1,11 @@
+package Chapter5;
+
+public class ForTick {
+
+    public static void main(String[] args) {
+
+        int n;
+        for (n = 10; n > 0; n--)
+            System.out.println("takt " + n);
+    }
+}
