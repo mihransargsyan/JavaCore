f7c78fef08ef6558da31e189be5627fb4096dc0b
mihransargsyan <misargsyan@gmail.com>
2021-10-17 20:56:54 +0400

    Add Homework

--- src/Homework/Arrayutil/ArrayUtil.java
+++ src/Homework/Arrayutil/ArrayUtil.java
@@ -7,113 +7,83 @@ public class ArrayUtil {
         int[] array = {71, 4, 7, 10, 138, 16, 175, 3, 54, 12};
 
 
-//        System.out.println(" 1 օրինակ ");
-//
-//        for (int i = 0; i < array.length; i++) {
-//
-//            System.out.println(array[i]);
-//        }
+        System.out.println(" 1 օրինակ ");
 
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
 
-//        System.out.println(" 2 օրինակ ");
-//
-//        int a = array[0];
-//
-//        for (int i = 0; i < array.length; i++) {
-//
-//            if (array[i] > a) {
-//
-//                a = array[i];
-//
-//            }
-//
-//        }
-//        System.out.println(a);
+        System.out.println(" 2 օրինակ ");
 
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println("max = " + max);
 
-//        System.out.println(" 3 օրինակ ");
-//
-//        int a = array[0];
-//
-//        for (int i = 0; i < array.length; i++) {
-//
-//            if (array[i] < a) {
-//
-//                a = array[i];
-//
-//            }
-//
-//        }
-//        System.out.println(a);
+        System.out.println(" 3 օրինակ ");
 
-//        System.out.println(" 4 օրինակ ");
-//
-//        for (int i = 0; i < array.length; i++) {
-//
-//            if (array[i] % 2 == 0) {
-//
-//                System.out.println(array[i]);
-//            }
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        System.out.println("min = " + min);
 
-//        System.out.println(" 5 օրինակ ");
-//
-//        for (int i = 0; i < array.length; i++) {
-//
-//            if (array[i] % 2 != 0) {
-//
-//                System.out.println(array[i]);
-//            }/
+        System.out.println(" 4 օրինակ ");
 
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.print(array[i] + " ");
+            }
+        }
+        System.out.println();
 
-//        System.out.println(" 6 օրինակ ");
-//
-//        int a = 0;
-//        for (int i = 0; i < array.length; i++) {
-//
-//            if (array[i] % 2 == 0) {
-//
-//                a++;
-//            }
-//
-//        }
-//        System.out.println(a);
+        System.out.println(" 5 օրինակ ");
 
-//        System.out.println(" 7 օրինակ ");
-//
-//        int a = 0;
-//
-//        for (int i = 0; i < array.length; i++) {
-//
-//            if (array[i] % 2 != 0) {
-//                a++;
-//            }
-//        }
-//        System.out.println(a);
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] % 2 != 0) {
+                System.out.print(array[j] + " ");
+            }
+        }
+        System.out.println();
 
-//        System.out.println(" 8 օրինակ ");
-//
-//        int a = 0;
-//
-//        for (int i = 0; i < array.length; i++) {
-//
-//            a = array[i] + a;
-//
-//        }
-//        System.out.println(a / array.length);
+        System.out.println(" 6 օրինակ ");
 
-//        System.out.println(" 9 օրինակ ");
-//
-//        int a = 0;
-//
-//        for (int i = 0; i < array.length; i++) {
-//
-//            a = array[i] + a;
-//
-//        }
-//        System.out.println(a);
+        int evenCount = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                evenCount++;
+            }
+        }
+        System.out.println("count of even: " + evenCount);
 
+        System.out.println(" 7 օրինակ ");
+
+        int oddCount = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                oddCount++;
+            }
+        }
+        System.out.println("odd of count: " + oddCount);
+
+        System.out.println(" 8,9 օրինակ ");
+
+        double sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        System.out.println("avg: " + sum / array.length);
+        System.out.println("sum: " + sum);
 
     }
 
 }
+
+
 
