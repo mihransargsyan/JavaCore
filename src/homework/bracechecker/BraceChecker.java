58b1fbd7f0661452f7d743a40e1017254e4f5971
mihransargsyan <misargsyan@gmail.com>
2021-11-09 21:53:46 +0400

    Add Homework

--- src/homework/bracechecker/BraceChecker.java
+++ src/homework/bracechecker/BraceChecker.java
@@ -22,26 +22,21 @@ public class BraceChecker {
                 case '[':
                     stack.push(c);
                     break;
-            }
-        }
-        for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            switch (c) {
                 case ')':
                     char chars = stack.pop();
-                    if (chars == '(') {
+                    if (chars != '(') {
                         System.err.println("Error: opened " + chars + " but closed " + c + " at " + i);
                     }
                     break;
                 case '}':
                     char chars1 = stack.pop();
-                    if (chars1 == '{') {
+                    if (chars1 != '{') {
                         System.err.println("Error: opened " + chars1 + " but closed " + c + " at " + i);
                     }
                     break;
                 case ']':
                     char chars2 = stack.pop();
-                    if (chars2 == '[') {
+                    if (chars2 != '[') {
                         System.err.println("Error: opened " + chars2 + " but closed " + c + " at " + i);
                     }
                     break;
